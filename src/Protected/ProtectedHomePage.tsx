19cdaad5f657ec93b05c14a636d5ac185904ce07
wira <wirayuda233@gmail.com>
2023-01-20 02:50:54 -0800

    fix toast run on first render

--- src/Protected/ProtectedHomePage.tsx
+++ src/Protected/ProtectedHomePage.tsx
@@ -4,11 +4,5 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 export const ProtectedHomePage: FC = () => {
   const uid = localStorage.getItem('user')
-
-  useEffect(() => {
-    if (uid === null) {
-      toast.error('You are unauthorize')
-    }
-  }, [uid])
   return uid ? <Outlet /> : <Navigate to='/signup' replace />;
 };
